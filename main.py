c607158f79aa508e12624db3f30dd88cc3a394f6
Gooonzalo <66415975+Gooonzalo@users.noreply.github.com>
2024-02-08 17:25:03 +0100

    Implemented data saves. - Data can now be saved from session to session. - Restructured data dictionaries. - Improved stile by adding cards and icons.

--- main.py
+++ main.py
@@ -5,129 +5,131 @@ from controllers import valves_controller
 from controllers import video_controller
 
 import mat73
+import pickle
+import os
 
 PORTS_MATRIX_DIR = 'OutputMatrixPorts12Batch.mat'
 
 
-def display_calibration_tab(calibration_vars):
-    calibration_vars.update(
-        {'reward_time': ui.number(label='Reward time', value=0.5, min=0, format='%.3f', precision=3)})
-
+def display_calibration_tab(data):
+    ui.number(label='Reward time', value=0.5, min=0, format='%.3f', step=0.001).bind_value(data, 'reward_time')
     ui.markdown('Compensation factor: ')
-    calibration_config = ui.row()
     f = valves_controller.compensationFactor
-    with calibration_config:
-        calibration_vars.update({'compensation_factor': (
-            ui.number(label='1', value=f[0], min=0, format='%.2f', precision=2),
-            ui.number(label='2', value=f[1], min=0, format='%.2f', precision=2),
-            ui.number(label='3', value=f[2], min=0, format='%.2f', precision=2),
-            ui.number(label='4', value=f[3], min=0, format='%.2f', precision=2),
-            ui.number(label='5', value=f[4], min=0, format='%.2f', precision=2),
-            ui.number(label='6', value=f[5], min=0, format='%.2f', precision=2),
-            ui.number(label='7', value=f[6], min=0, format='%.2f', precision=2),
-            ui.number(label='8', value=f[7], min=0, format='%.2f', precision=2))})
-
-    ui.markdown('Active ports: ')
-    calibration_vars.update({'active_ports': ui.row()})
-    with calibration_vars['active_ports']:
-        ui.switch('1', value=True)
-        ui.switch('2', value=True)
-        ui.switch('3', value=True)
-        ui.switch('4', value=True)
-        ui.switch('5', value=True)
-        ui.switch('6', value=True)
-        ui.switch('7', value=True)
-        ui.switch('8', value=True)
-
-
-def display_experiment_tab(mode_vars, experiment_vars):
-    mode_vars.update({'config': ui.toggle({1: 'Train', 2: 'Recall'}, value=1)})
     with ui.row():
-        experiment_vars.update({'exp_day': ui.number(label='Experiment day', value=1, min=1, format='%.0f'),
-                                'animal_number': ui.number(label='Animal Number', value=1, min=1,
-                                                           format='%.0f'),
-                                'stage': ui.select({1: 'Stage 1', 2: 'Stage 2', 3: 'Stage 3'}, label='Stage',
-                                                   value=3),
-                                'drug_type': ui.number(label='Drug type', value=0, min=0, format='%.0f'),
-                                'dose': ui.number(label='Dose', value=0.000, min=0, format='%.3f',
-                                                  precision=3)})
-    experiment_vars.update({'correct_port': ui.label('Correct port: '),
-                            'yesterday_port': ui.label('Yesterday port: ')})
+        for i in range(0, 7):
+            ui.number(label=str(i + 1), value=f[i], min=0, format='%.2f', step=0.01)
+    ui.markdown('Active ports: ')
+    with ui.row():
+        for i in range(0, 7):
+            ui.switch(str(i + 1), value=True)
+
+
+def display_experiment_tab(data):
+    with ui.row():
+        ui.number(label='Experiment day', value=1, min=1, format='%.0f').bind_value(data, 'exp_day')
+        ui.number(label='Animal Number', value=1, min=1, format='%.0f').bind_value(data, 'animal_number')
+        ui.select({1: 'Stage 1', 2: 'Stage 2', 3: 'Stage 3'}, label='Stage', value=3).bind_value(data, 'stage')
+        ui.number(label='Drug type', value=0, min=0, format='%.0f').bind_value(data, 'drug_type')
+        ui.number(label='Dose', value=0.000, min=0, format='%.3f', step=0.001).bind_value(data, 'dose')
+    with ui.grid(columns=2):
+        ui.markdown('Correct port:')
+        with ui.card():
+            ui.label('').bind_text(data, 'correct_port')
+        ui.markdown('Yesterday\'s port:')
+        with ui.card():
+            ui.label('').bind_text(data, 'yesterdays_port')
 
 
 def display_gui():
-    ui.markdown('## Behavioral Boxes GUI')
-    ui.markdown('### Enter the desired configuration and select the running mode:')
+    data = load_data()
 
-    experiment_vars = {}
-    calibration_vars = {}
-
+    ui.markdown('### Behavioral Boxes GUI')
+    ui.markdown('Enter the desired configuration and select the running mode:')
     with ui.row():
-        mode_vars = {'box': ui.select({1: 'Box 1', 2: 'Box 2', 3: 'Box 3'}, label='Box', value=1)}
-        ui.button('Save', on_click=lambda: save_values(mode_vars, experiment_vars, calibration_vars))
-        ui.button('Run', on_click=lambda: run_experiment(mode_vars, experiment_vars, calibration_vars))
-
-    mode_vars.update({'mode': ui.tabs()})
-    with mode_vars['mode'] as tabs:
+        with ui.card():
+            with ui.row():
+                ui.select({1: 'Box 1', 2: 'Box 2', 3: 'Box 3'}, label='Box', value=1).bind_value(data, 'box')
+                ui.select({0: 'Calibrate', 1: 'Train', 2: 'Recall'}, label='Mode', value=1).bind_value(data, 'mode')
+        with ui.column():
+            ui.button('Save', icon='save', on_click=lambda: save_data(data))
+            ui.button('Run', icon='play_circle_outline', on_click=lambda: run_experiment(data))
+    ui.separator()
+    with ui.tabs() as tabs:
         calibrate = ui.tab('Calibration')
-        experiment = ui.tab('Train and recall')
-        mode_vars['mode'].value = 'Train and recall'
+        experiment = ui.tab('Train or recall')
     with ui.tab_panels(tabs, value=experiment):
         with ui.tab_panel(calibrate):
-            display_calibration_tab(calibration_vars)
+            with ui.card():
+                display_calibration_tab(data)
         with ui.tab_panel(experiment):
-            display_experiment_tab(mode_vars, experiment_vars)
+            with ui.card():
+                display_experiment_tab(data)
 
 
-def update_ports(option_vars):
+def update_ports(data):
     port_matrix = mat73.loadmat(PORTS_MATRIX_DIR)
-    correct_port = port_matrix['A'][main_controller.ExpDay - 1][main_controller.AnimalNumber - 1]
-    option_vars['correct_port'].text = 'Correct port: ' + str(correct_port)
-    if main_controller.ExpDay > 1:
-        yesterday_port = port_matrix['A'][main_controller.ExpDay - 2][main_controller.AnimalNumber - 1]
+    day = int(data['exp_day'])
+    animal = int(data['animal_number'])
+    if day > len(port_matrix['A']):
+        ui.notify('Experiment day out of range.')
+        data['correct_port'] = '-'
+        data['yesterdays_port'] = '-'
+    elif animal > len(port_matrix['A'][day - 1]):
+        ui.notify('Animal number out of range.')
+        data['correct_port'] = '-'
+        data['yesterdays_port'] = '-'
     else:
-        yesterday_port = 'First day'
-    option_vars['yesterday_port'].text = 'Yesterday port: ' + str(yesterday_port)
+        data['correct_port'] = port_matrix['A'][day - 1][animal - 1]
+        if main_controller.ExpDay > 1:
+            data['yesterdays_port'] = port_matrix['A'][day - 2][animal - 1]
+        else:
+            data['yesterdays_port'] = 'First day'
 
 
-def save_values(mode_vars, experiment_vars, calibration_vars):
-    mode = mode_vars['mode'].value
-    if mode == 'Calibration':
-        valves_controller.timeLengthWReward = calibration_vars['reward_time'].value
+def save_data(data):
+    if data['mode'] == 0:
+        valves_controller.timeLengthWReward = data['reward_time']
+        valves_controller.compensationFactor = data['compensation_factor']
+        ui.notify(data['compensation_factor'])
 
-        compensation_factor = [0] * 8
-        for i in range(0, len(compensation_factor)):
-            compensation_factor[i] = calibration_vars['compensation_factor'][i].value
-        valves_controller.compensationFactor = compensation_factor
-
-        ui.notify(compensation_factor)
-
-    elif mode == 'Train and recall':
-        main_controller.ExpDay = int(experiment_vars['exp_day'].value)
-        main_controller.AnimalNumber = int(experiment_vars['animal_number'].value)
-        main_controller.StageLevel = experiment_vars['stage'].value
-        main_controller.DrugType = experiment_vars['drug_type'].value
-        main_controller.Dose = experiment_vars['dose'].value
-        update_ports(experiment_vars)
+    elif data['mode'] == 1 or data['mode'] == 2:
+        main_controller.ExpDay = int(data['exp_day'])
+        main_controller.AnimalNumber = int(data['animal_number'])
+        main_controller.StageLevel = data['stage']
+        main_controller.DrugType = data['drug_type']
+        main_controller.Dose = data['dose']
+        update_ports(data)
         ui.notify('Saved')
     else:
         ui.notify('ERROR')
 
+    with open('data.pkl', 'wb') as file:
+        pickle.dump(data, file)
 
-def run_experiment(mode_vars, experiment_vars, calibration_vars):
-    mode = mode_vars['mode'].value
-    box = mode_vars['box'].value
-    config = mode_vars['config'].value
-    if mode == 'Calibration':
-        valves_controller.configure(box)
+
+def load_data() -> dict:
+    if os.path.exists('data.pkl'):
+        with open('data.pkl', 'rb') as file:
+            data = pickle.load(file)
+    else:
+        data = {'box': 1, 'mode': 1, 'correct_port': '-', 'yesterdays_port': '-',
+                'exp_day': 1, 'animal_number': 1, 'stage': 3, 'drug_type': 0, 'dose': 0.00,
+                'reward_time': 0.5, 'active_ports': [False] * 8,
+                'compensation_factor': [1.25, 2., .83, 2.9, 2.03, .83, 2.63, .8]}
+    return data
+
+
+def run_experiment(data):
+    if data['mode'] == 0:
+        valves_controller.configure(data['box'])
         ui.notify('Ready to calibrate!')
     else:
-        sound_controller.configure(box)
-        main_controller.configure(box, mode)
-        video_controller.configure(box, mode)
-        if config == 1:
+        sound_controller.configure(data['box'])
+        main_controller.configure(data['box'], data['mode'])
+        video_controller.configure(data['box'], data['mode'])
+        if data['mode'] == 1:
             ui.notify('Train mode!')
-        elif config == 2:
+        elif data['mode'] == 2:
             ui.notify('Recall mode!')
         else:
             ui.notify('ERROR')
